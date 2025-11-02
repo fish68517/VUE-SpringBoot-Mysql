6a110577dfd0237a3df6f428a73d1d97271b0465
fish68517 <fish68517@gmail.com>
2025-11-02 20:53:04 +0800

    1

--- Android/app/src/main/java/com/archive/app/ApiClient.java
+++ Android/app/src/main/java/com/archive/app/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.1.10:8081/";
+    private static final String BASE_URL = "http://192.168.2.185:8081/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
